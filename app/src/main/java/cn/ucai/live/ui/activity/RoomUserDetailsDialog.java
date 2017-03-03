214bba4620a377da80e781c1521d5bd513e023dc
zhangqiang <1126959509@qq.com>
2017-03-03 10:39:19 +0800

    观众详情弹框显示头像

--- app/src/main/java/cn/ucai/live/ui/activity/RoomUserDetailsDialog.java
+++ app/src/main/java/cn/ucai/live/ui/activity/RoomUserDetailsDialog.java
@@ -14,6 +14,10 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.hyphenate.easeui.utils.EaseUserUtils;
+import com.hyphenate.easeui.widget.EaseImageView;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -25,87 +29,98 @@ import cn.ucai.live.R;
  */
 public class RoomUserDetailsDialog extends DialogFragment {
 
-  Unbinder unbinder;
-  @BindView(R.id.tv_username) TextView usernameView;
-  @BindView(R.id.btn_mentions) Button mentionBtn;
+    Unbinder unbinder;
+    @BindView(R.id.tv_username)
+    TextView usernameView;
+    @BindView(R.id.btn_mentions)
+    Button mentionBtn;
+    @BindView(R.id.dialog_user_avatar)
+    EaseImageView mDialogUserAvatar;
 
-  private String username;
+    private String username;
 
-  public static RoomUserDetailsDialog newInstance(String username) {
-    RoomUserDetailsDialog dialog = new RoomUserDetailsDialog();
-    Bundle args = new Bundle();
-    args.putString("username", username);
-    dialog.setArguments(args);
-    return dialog;
-  }
-
-  @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_room_user_details, container, false);
-    unbinder = ButterKnife.bind(this, view);
-    return view;
-  }
-
-  @Override public void onActivityCreated(Bundle savedInstanceState) {
-    super.onActivityCreated(savedInstanceState);
-    if (getArguments() != null) {
-      username = getArguments().getString("username");
+    public static RoomUserDetailsDialog newInstance(String username) {
+        RoomUserDetailsDialog dialog = new RoomUserDetailsDialog();
+        Bundle args = new Bundle();
+        args.putString("username", username);
+        dialog.setArguments(args);
+        return dialog;
     }
-    if (username != null) {
-      usernameView.setText(username);
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_room_user_details, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
-    mentionBtn.setText("@TA");
-  }
 
-  @OnClick(R.id.btn_message) void onMessageBtnClick(){
-    ChatFragment fragment = ChatFragment.newInstance(username, false);
-    dismiss();
-    getActivity().getSupportFragmentManager().beginTransaction().addToBackStack(null).replace(R.id.message_container, fragment).commit();
-  }
-
-  @OnClick(R.id.btn_mentions) void onMentionBtnClick(){
-    if(dialogListener != null){
-      dialogListener.onMentionClick(username);
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getArguments() != null) {
+            username = getArguments().getString("username");
+        }
+        if (username != null) {
+            usernameView.setText(username);
+        }
+        EaseUserUtils.setAppUserAvatar(getContext(),username,mDialogUserAvatar);
+        mentionBtn.setText("@TA");
     }
-  }
 
-  @OnClick(R.id.btn_follow) void onFollowBtnClick(){
-  }
+    @OnClick(R.id.btn_message)
+    void onMessageBtnClick() {
+        ChatFragment fragment = ChatFragment.newInstance(username, false);
+        dismiss();
+        getActivity().getSupportFragmentManager().beginTransaction().addToBackStack(null).replace(R.id.message_container, fragment).commit();
+    }
 
-  private UserDetailsDialogListener dialogListener;
+    @OnClick(R.id.btn_mentions)
+    void onMentionBtnClick() {
+        if (dialogListener != null) {
+            dialogListener.onMentionClick(username);
+        }
+    }
 
-  public void setUserDetailsDialogListener(UserDetailsDialogListener dialogListener){
-    this.dialogListener = dialogListener;
-  }
+    @OnClick(R.id.btn_follow)
+    void onFollowBtnClick() {
+    }
 
-  interface UserDetailsDialogListener{
-    void onMentionClick(String username);
-  }
+    private UserDetailsDialogListener dialogListener;
+
+    public void setUserDetailsDialogListener(UserDetailsDialogListener dialogListener) {
+        this.dialogListener = dialogListener;
+    }
+
+    interface UserDetailsDialogListener {
+        void onMentionClick(String username);
+    }
 
 
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // 使用不带theme的构造器，获得的dialog边框距离屏幕仍有几毫米的缝隙。
+        // Dialog dialog = new Dialog(getActivity());
+        Dialog dialog = new Dialog(getActivity(), R.style.room_user_details_dialog);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // must be called before set content
+        dialog.setContentView(R.layout.fragment_room_user_details);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCanceledOnTouchOutside(true);
 
-  @Override
-  public Dialog onCreateDialog(Bundle savedInstanceState) {
-    // 使用不带theme的构造器，获得的dialog边框距离屏幕仍有几毫米的缝隙。
-    // Dialog dialog = new Dialog(getActivity());
-    Dialog dialog = new Dialog(getActivity(), R.style.room_user_details_dialog);
-    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // must be called before set content
-    dialog.setContentView(R.layout.fragment_room_user_details);
-    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-    dialog.setCanceledOnTouchOutside(true);
+        // 设置宽度为屏宽、靠近屏幕底部。
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams wlp = window.getAttributes();
+        wlp.gravity = Gravity.BOTTOM;
+        wlp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        window.setAttributes(wlp);
 
-    // 设置宽度为屏宽、靠近屏幕底部。
-    Window window = dialog.getWindow();
-    WindowManager.LayoutParams wlp = window.getAttributes();
-    wlp.gravity = Gravity.BOTTOM;
-    wlp.width = WindowManager.LayoutParams.MATCH_PARENT;
-    window.setAttributes(wlp);
+        return dialog;
+    }
 
-    return dialog;
-  }
-
-  @Override public void onDestroyView() {
-    super.onDestroyView();
-    unbinder.unbind();
-  }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
